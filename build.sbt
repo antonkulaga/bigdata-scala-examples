8dbc38f9d48da1a9de0831ceeb90a8fa54448be9
antonkulaga <antonkulaga@gmail.com>
2014-05-14 22:51:52 +0300

    BlankNode bug

--- build.sbt
+++ build.sbt
@@ -2,6 +2,7 @@ name := "bigdata-scala-examples"
 
 version := "0.1"
 
+scalaVersion := "2.11.0"
 
 // For apache libs
 resolvers += "apache-repo-releases" at "http://repository.apache.org/content/repositories/releases/"   
@@ -14,6 +15,6 @@ resolvers += "nxparser-repo" at "http://nxparser.googlecode.com/svn/repository/"
 
 libraryDependencies ++= Seq(
     "com.bigdata" % "bigdata" % "1.3.0",
-    "org.scalatest" % "scalatest_2.10" % "2.1.0"
+    "org.scalatest" %% "scalatest" % "2.2.0-M1"
 )
  
--- src/test/scala/simple/SimpleBigDataTestSpec.scala
+++ src/test/scala/simple/SimpleBigDataTestSpec.scala
@@ -9,12 +9,13 @@ import org.scalatest.matchers.ShouldMatchers
 
 
 import scala.collection.immutable.{Map, List}
-import org.openrdf.model.{Statement, Value}
+import org.openrdf.model.{BNode, Statement, Value}
 import collection.JavaConversions._
 import org.openrdf.repository.RepositoryResult
 import org.scalatest.WordSpecLike
 import org.scalatest.Matchers
 import org.scalatest.BeforeAndAfterAll
+import org.openrdf.model.impl.{BNodeImpl, URIImpl, StatementImpl}
 
 
 /**
@@ -70,6 +71,52 @@ class SimpleBigDataTestSpec  extends  WordSpec with Matchers with SimpleTestData
      val queryFreeze= db.select(wrongQuery)
      queryFreeze.isFailure shouldBe true
      db.shutDown() // shutting down
+
+    }
+
+    //HERE IS A TEST FOR BNODE BUG
+    "work with BlankNodes well" in {
+
+      val db = BigData(true) //cleaning the files and initializing the database
+
+      //let's write one statement with bnode into the database
+      val con = db.writeConnection
+      con.setAutoCommit(false)
+      val bnode = con.getValueFactory.createBNode()
+      val prop = new URIImpl("http://foo/property")
+      val obj = new URIImpl("http://bar/object")
+      val stm = new StatementImpl(bnode,prop,obj)
+
+      con.add(stm)
+
+      con.commit()
+      con.close()
+
+      //lest open new connection to read it
+      val con2 = db.readConnection
+      con2.hasStatement(null,prop,obj,true) shouldEqual true
+      val bnode2: BNode = con2.getStatements(null,prop,obj,true)
+        .collectFirst{
+          case st if st.getSubject.isInstanceOf[BNode]=>
+            st.getSubject.asInstanceOf[BNode]
+        }.get  //get the blank node
+
+
+      bnode2 shouldEqual bnode
+
+      //let's create blank node implementation with the same id (here I use my own)
+      val newBNode = new BNodeImpl(bnode2.getID)
+      newBNode shouldEqual bnode2 //check that new and old bnodes are equal
+
+      con2.hasStatement(bnode2,prop,obj,true) shouldEqual true
+      con2.hasStatement(newBNode,prop,obj,true) shouldEqual true //HERE IS THE BUG!
+
+      con2.close()
+
+
+
+
+      db.shutDown() // shutting down
 
     }
   }
